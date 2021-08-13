1317327bcdcfed07ae7ad9742992fe15a2490e62
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-08-13 18:01:58 +0200

    Test for labels with long messages

--- vars/configureConanRemotes.groovy
+++ vars/configureConanRemotes.groovy
@@ -2,7 +2,7 @@ def call()
 {
 	withCredentials([string(credentialsId: 'ArtifactoryAPIKey', variable: 'CONAN_API_KEY')])
 	{
-		sh label: "Configure conan remotes", script: """
+		sh label: "Configure conan remotes with a very very very very very very very very very very long message", script: """
 			conan remote clean
 			conan remote add systelab-conan https://artifactory.systelab.net/artifactory/api/conan/systelab-conan --force
 			conan remote add conan-center-remote https://artifactory.systelab.net/artifactory/api/conan/conan-center-remote --force

00c2b9fe6a491abf8db48c5d441ed9393814b86f
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-08-13 17:46:27 +0200

    Added label to pipeline step

--- vars/configureConanRemotes.groovy
+++ vars/configureConanRemotes.groovy
@@ -2,7 +2,7 @@ def call()
 {
 	withCredentials([string(credentialsId: 'ArtifactoryAPIKey', variable: 'CONAN_API_KEY')])
 	{
-		sh '''
+		sh label: "Configure conan remotes for C++ task force", script: '''
 			conan remote clean
 			conan remote add systelab-conan https://artifactory.systelab.net/artifactory/api/conan/systelab-conan --force
 			conan remote add conan-center-remote https://artifactory.systelab.net/artifactory/api/conan/conan-center-remote --force
@@ -14,6 +14,6 @@ def call()
 
 			conan remote list
 			conan user
-		'''
+		''', 
 	}
 }

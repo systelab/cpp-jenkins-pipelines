68fdf20a966f3303f744f9200195d515b82c3c9b
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2021-09-29 09:33:17 +0200

    Updated to take username from Jenkins credentials (agnostic to server used)

--- vars/configureConanRemotes.groovy
+++ vars/configureConanRemotes.groovy
@@ -1,19 +1,22 @@
 def call()
 {
-	withCredentials([string(credentialsId: 'ArtifactoryAPIKey', variable: 'CONAN_API_KEY')])
+	withCredentials([string(credentialsId: 'ArtifactoryUsername', variable: 'CONAN_USERNAME')])
 	{
-		sh label: "Configure conan remotes", script: """
-			conan remote clean
-			conan remote add systelab-conan https://artifactory.systelab.net/artifactory/api/conan/systelab-conan --force
-			conan remote add conan-center-remote https://artifactory.systelab.net/artifactory/api/conan/conan-center-remote --force
-			conan remote add systelab-conan-local https://artifactory.systelab.net/artifactory/api/conan/systelab-conan-local --force
+		withCredentials([string(credentialsId: 'ArtifactoryAPIKey', variable: 'CONAN_API_KEY')])
+		{
+			sh label: "Configure conan remotes", script: """
+				conan remote clean
+				conan remote add systelab-conan https://artifactory.systelab.net/artifactory/api/conan/systelab-conan --force
+				conan remote add conan-center-remote https://artifactory.systelab.net/artifactory/api/conan/conan-center-remote --force
+				conan remote add systelab-conan-local https://artifactory.systelab.net/artifactory/api/conan/systelab-conan-local --force
 
-			conan user -p ${CONAN_API_KEY} -r systelab-conan newton-ci
-			conan user -p ${CONAN_API_KEY} -r conan-center-remote newton-ci
-			conan user -p ${CONAN_API_KEY} -r systelab-conan-local newton-ci
+				conan user -p ${CONAN_API_KEY} -r systelab-conan ${CONAN_USERNAME}
+				conan user -p ${CONAN_API_KEY} -r conan-center-remote ${CONAN_USERNAME}
+				conan user -p ${CONAN_API_KEY} -r systelab-conan-local ${CONAN_USERNAME}
 
-			conan remote list
-			conan user
-		"""
+				conan remote list
+				conan user
+			"""
+	}
 	}
 }
